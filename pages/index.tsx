6ed006b674eb96ad37c21a0cbfa1d73c77a1025f
brunoohsilvaa <95055355+brunoohsilvaa@users.noreply.github.com>
2021-12-08 10:12:32 -0300

    v0.1

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next' 
+import { Login} from '../containers/Login'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Login/>
+    </>
+  )
+}
+
+export default Home
